923ec296912de02851139b7f81d8b0063a30c1bd
EC2 Default User <ec2-user@ip-172-31-19-86.us-east-2.compute.internal>
2018-08-06 05:28:06 +0000

    modify

--- node/server.js
+++ node/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-	res.send("<html><head><h1>hostname : " + hostname + " </h1></head><body><img src='https://blog.docker.com/wp-content/uploads/Swarmnado-357x627-30-1.gif'></body></html>");
+	res.send("<html><head><h1>hostname : " + hostname + " </h1></head><body>image<img src='https://blog.docker.com/wp-content/uploads/Swarmnado-357x627-30-1.gif'></body></html>");
 });
 
 app.listen(PORT, HOST);
